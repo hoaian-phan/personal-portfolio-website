e10efa499993be828f7bf974262ce89f782edc39
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-18 07:03:26 +0000

    Completed Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import CTA from './CTA'
+import me from '../../assets/me.jpeg'
+import Connect from './connect'
 
 const Header = () => {
   return (
@@ -9,6 +11,10 @@ const Header = () => {
         <h1> Hoai-An Phan</h1>
         <h5> Full-stack Software Engineer</h5>
         <CTA />
+        <Connect />
+        <div>
+          <img src={me} alt="me" />
+        </div>
 
       </div>
 
---
+++ src/components/header/connect.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {GrLinkedin} from 'react-icons/gr'
+import {GrGithub} from 'react-icons/gr'
+
+const connect = () => {
+  return (
+    <div>
+        <a href="https://linkedin.com/in/hoai-an-phan" target="_blank"><GrLinkedin></GrLinkedin></a>
+        <a href="https://github.com/hoaian-phan" target="_blank"><GrGithub></GrGithub></a>
+        
+    </div>
+  )
+}
+
+export default connect

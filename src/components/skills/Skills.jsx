88feb18a0fc19231ec855099fa13ab617c0c1c75
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-18 19:13:40 +0000

    Completed Skills section. Restarted Experience section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -2,7 +2,105 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <section id='skills'>Skills</section>
+    <section id='experience'>
+      <h5> What I Know</h5>
+      <h2>My Technical Skills</h2>
+
+      <div>
+        <div>
+          <h3>Languages</h3>
+            <div>
+              <article>
+                <h4>Python</h4>
+              </article>
+
+              <article>
+                <h4>JavaScript</h4>
+              </article>
+
+              <article>
+                <h4>C++</h4>
+              </article>
+
+              <article>
+                <h4>SQL</h4>
+              </article>
+
+              <article>
+                <h4>HTML</h4>
+              </article>
+
+              <article>
+                <h4>CSS</h4>
+              </article>
+            </div>
+        </div>
+
+        <div>
+          <h3>Libraries and Frameworks</h3>
+            <div>
+              <article>
+                <h4>Flask</h4>
+              </article>
+
+              <article>
+                <h4>SQLAlchemy</h4>
+              </article>
+
+              <article>
+                <h4>Celery</h4>
+              </article>
+
+              <article>
+                <h4>React</h4>
+              </article>
+
+              <article>
+                <h4>Jinja</h4>
+              </article>
+
+              <article>
+                <h4>Bootstrap</h4>
+              </article>
+
+              <article>
+                <h4>PostgreSQL</h4>
+              </article>
+            </div>
+        </div>
+
+        <div>
+          <h3>Industry Tools</h3>
+            <div>
+              <article>
+                <h4>Git / GitHub</h4>
+              </article>
+
+              <article>
+                <h4>Shell / Command line</h4>
+              </article>
+
+              <article>
+                <h4>Docker</h4>
+              </article>
+
+              <article>
+                <h4>Redis</h4>
+              </article>
+
+              <article>
+                <h4>AWS Cloud</h4>
+              </article>
+
+              <article>
+                <h4>Heroku</h4>
+              </article>
+            </div>
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 

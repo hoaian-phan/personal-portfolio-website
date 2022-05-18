7182ea60616759b5a0dfe2650d18b422b0d6cbda
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-18 21:55:41 +0000

    Completed Portfolio section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -8,39 +8,39 @@ const Experience = () => {
         <div>
           <ul>
             <li>
-              <div>
+              <article>
                 <time>February 2022 - April 2022</time>
                 <h4>HackBright Academy</h4>
                 <p>Software Engineering Fellowship</p>
                 <p>Collaborated on pair programming with cohortmates on daily lab projects. Built and deployed a web application on AWS using skilss and knowledge across fundamentals of data structures, algorithms, database, full-stack and testing</p>
-              </div>
+              </article>
             </li>
 
             <li>
-              <div>
+              <article>
                 <time>November 2017 - June 2021</time>
                 <h4>Standard Chartered Bank, North America</h4>
                 <p>Sanctions Filtering Associate</p>
                 <p>Identified and investigated possible matches related to sanctioned parties and countries, money laundering and terrorist financing organizations.</p>
-              </div>
+              </article>
             </li>
 
             <li>
-              <div>
+              <article>
                 <time>March 2014 - March 2016</time>
                 <h4>Masan Consumer Corporation</h4>
                 <p>Executive Assistant</p>
                 <p>Arranged and managed CEO's meeting calendar. Coordinated across various stakeholders to assist CEO in business operation.</p>
-              </div>
+              </article>
             </li>
 
             <li>
-              <div>
+              <article>
                 <time>April 2011 - December 2013</time>
                 <h4>EQuest Academy</h4>
                 <p>Academic Course Facilitator</p>
                 <p>Built curriculum, recruited and trained English teachers.</p>
-              </div>
+              </article>
             </li>
           </ul>
         </div>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import img from '../../assets/playdatebirdies.png'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>What I Created</h5>
+      <h2>Portfolio</h2>
+
+      <div>
+        <article>
+          <div>
+            <div>
+              <img src={img} alt="playdate birdies"/>
+            </div>
+            <h3>Playdate Birdies Web Application</h3>
+            <p>Technologies: Python, Flask, JavaScript, HTML, Bootstrap, Google Map APIs, Sendgrid APIs, ASW Lightsail</p>
+            <a href="https://www.youtube.com/watch?v=u-X1LhxQtec" target="_blank" rel="noreferrer">Live Demo</a>
+            <a href="https://github.com/hoaian-phan/project-playdate-birdies" target="_blank" rel="noreferrer">GitHub</a>
+            <a href="http://playdatebirdies.com/" target="_blank" rel="noreferrer">Visit the site</a>
+          </div>
+        </article>
+      </div>
+
+
+
+    </section>
   )
 }
 

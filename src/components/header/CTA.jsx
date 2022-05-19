0cb7228a2975512210802deab12821b80175f0cf
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-19 06:11:07 +0000

    Finished styling Header

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,14 +3,13 @@ import resume from '../../assets/resume.pdf'
 
 const CTA = () => {
   return (
-    <div>
-      <button type="button" class="btn btn-light bg-navy">
-        <a href={resume} download class="text-light-blue ">Download resume</a>
+    <div class="mt-5">
+      <button type="button" class="btn btn-light bg-navy me-1">
+        <a href={resume} download class="text-light-blue text-decoration-none">Download resume</a>
       </button>
-      <button type="button" class="btn btn-info">
-        <a href="#contact" class="text-navy">Let's Chat</a>
+      <button type="button" class="btn btn-light bg-light-blue border border-white ms-1">
+        <a href="#contact" class="text-navy text-decoration-none">Let's Chat</a>
       </button>
-
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import CTA from './CTA'
-import me from '../../assets/me.jpeg'
-import Connect from './connect'
+import me from '../../assets/me_full.JPG'
 
 const Header = () => {
   return (
-    <header class="container">
+    <header class="container my-5">
       <div class="text-center">
         <h6> Hello I'm</h6>
         <h1> Hoai-An Phan</h1>
-        <h6> Full-stack Software Engineer</h6>
-        <CTA />
-        <Connect />
-        <div>
-          <img src={me} alt="me" />
+        <h6> Software Engineer &nbsp;&nbsp; • &nbsp;&nbsp; Gardener &nbsp;&nbsp; • &nbsp;&nbsp; Coffee Lover</h6>
+        <div class="my-4">
+          <img src={me} class="img-thumbnail rounded resize-photo" alt="me" />
         </div>
-
+        <CTA />
+        
       </div>
-
     </header>
   )
 }

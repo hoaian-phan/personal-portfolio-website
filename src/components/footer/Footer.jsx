0a0b9d760ee83329a5cf0e1cb011c4aa6c9c2ece
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-19 05:36:18 +0000

    Completed Footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import {GrLinkedin} from 'react-icons/gr'
+import {GrGithub} from 'react-icons/gr'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#">HOAI-AN PHAN</a>
+
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#skills">Technical skills</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div>
+      <a href="https://linkedin.com/in/hoai-an-phan" target="_blank" rel="noreferrer"><GrLinkedin></GrLinkedin></a>
+        <a href="https://github.com/hoaian-phan" target="_blank" rel="noreferrer"><GrGithub></GrGithub></a>
+      </div>
+    </footer>
   )
 }
 

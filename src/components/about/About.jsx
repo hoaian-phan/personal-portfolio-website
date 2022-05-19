d6fa020e26eef51445073d24b9f6e01062c56a05
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-19 20:10:31 +0000

    Finished styling About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -7,58 +7,56 @@ const About = () => {
       <h6 class="text-center">I'm excited to tell you</h6>
       <h2 class="text-center"> About Me</h2>
 
-      <div class="m-5">
-        
-          <div>
+      <div class="container m-5">
+        <div class="row">
+          <div class="col-12 col-xxl-4 text-center">
             <img src={me} class="img-thumbnail resize-photo" alt="About me" />
           </div>
         
-
-        <div class="container">
-          <div class="d-inline p-5">
-            <article class="border shadow bg-navy border-navy pt-3 pt-2">
-              <h5 class="text-center">Who I Am</h5>
-                <ul>
-                  <li>An aspiring full-stack software engineer</li>
-                  <li>A lifetime learner of technologies</li>
-                  <li>A proud mom of my wonderful toddler</li>
-                </ul>
-            </article>
-            
+          <div class="col-12 col-xxl-4">
+            <div class="row d-flex align-items-center justify-content-center">
+              <article class="border shadow bg-navy border-navy pt-3 m-4">
+                <h5 class="text-center">Who I Am</h5>
+                  <ul>
+                    <li>An aspiring full-stack software engineer</li>
+                    <li>A lifetime learner of technologies</li>
+                    <li>A proud mom of my wonderful toddler</li>
+                  </ul>
+              </article>
+              
+              <article class="border shadow bg-navy border-navy pt-3">
+                <h5 class="text-center">What I Do</h5>
+                  <ul>
+                    <li>Deliver solutions that excite customers </li>
+                    <li>Use software to solve non-software problems </li>
+                    <li>Create and design with passion</li>
+                  </ul>
+              </article>
+            </div>
           </div>
-          <div class="d-inline p-2">
-            <article class="border shadow bg-navy border-navy pt-3 pt-2">
-              <h5 class="text-center">What I Do</h5>
-                <ul>
-                  <li>Deliver solutions that excite customers </li>
-                  <li>Use software to solve non-software problems </li>
-                  <li>Create and design with passion</li>
-                </ul>
-            </article>
-          </div>
-        </div>
+        
 
-        <div class="container">
-          <div class="d-inline p-2">
-            <article class="border shadow bg-navy border-navy pt-3 pt-2">
+          <div class="col-12 col-xxl-4">
+            <div class="row d-flex align-items-center justify-content-center">
+              <article class="border shadow bg-navy border-navy pt-3 m-4">
               <h5 class="text-center">What I Believe</h5>
                 <ul>
-                  <li>I believe in self-motivation and determination</li>
+                  <li>I believe in self-motivation &#38; determination</li>
                   <li>I believe in the power of constructive feedback</li>
                   <li>I believe in getting things done </li>
                 </ul>
-            </article>
-          </div>
+              </article>
+              
 
-          <div class="d-inline p-2">
-            <article class="border shadow bg-navy border-navy pt-3 pt-2">
-              <h5 class="text-center">How I Learn</h5>
-                <ul>
-                  <li>I accept and get comfortable with the unknowns</li>
-                  <li>I get excited about learning new things</li>
-                  <li>I ask questions. It's the best way to learn and grow</li>
-                </ul>
-            </article>
+              <article class="border shadow bg-navy border-navy pt-3">
+                <h5 class="text-center">How I Learn</h5>
+                  <ul>
+                    <li>I accept &#38; get comfortable with the unknowns</li>
+                    <li>I get excited about learning new things</li>
+                    <li>I ask questions. It's the best way to learn &#38; grow</li>
+                  </ul>
+              </article>
+            </div>
           </div>
         </div>
       </div>

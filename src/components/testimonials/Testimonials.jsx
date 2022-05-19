7bbe6b9633971b6573a05b1d8f5b5b54ed515f8c
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-19 05:36:40 +0000

    Completed Testimonial section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import avatar from '../../assets/me_park.JPEG'
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>What others think about me and my work</h5>
+      <h2> Testimonials</h2>
+
+      <div>
+        <article>
+          <div>
+            <img src={avatar} alt="avatar" />
+          </div>
+          <h5>Na Phan</h5>
+            <small>
+              A smart learner
+            </small>
+        </article>
+      </div>
+
+
+    </section>
   )
 }
 

29d0c6c81e98d7faddc0457e0fe5eb53ddcf8ad3
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-23 21:57:20 +0000

    Fixed fail to view pdf error for resume download

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import HoaiAnPhanResume from '../../assets/Hoai-An-Phan_Resume_2022.pdf'
+import HoaiAnPhanResume from '../../Hoai-An-Phan_Resume_2022.pdf'
 
 const CTA = () => {
   return (
     <div class="my-5">
       <button type="button" class="btn btn-light bg-navy me-1">
-        <a href={HoaiAnPhanResume} download class="text-light-blue text-decoration-none">Download Resume</a>
+      <a href={HoaiAnPhanResume} target = "_blank" rel="noreferrer">Download Resume</a>
       </button>
       <button type="button" class="btn btn-light bg-light-blue border border-white ms-1">
-        <a href="#contact" class="text-navy text-decoration-none">Let's Chat</a>
+        <a href="#contact">Let's Chat</a>
       </button>
     </div>
   )

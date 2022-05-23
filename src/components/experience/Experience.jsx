6be2c845a7bd56941e060d135fe1c09879304e43
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-23 22:08:43 +0000

    Changed Experience details

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -18,7 +18,8 @@ const Experience = () => {
                     <h5 class="text-light-blue">Feb 2022 - Apr 2022</h5>
                     <h4>HackBright Academy</h4>
                     <p>Software Engineering Fellowship</p>
-                    <p>Collaborated on pair programming with cohortmates on daily lab projects. Built and deployed a web application on AWS using skills and knowledge across fundamentals of data structures, algorithms, database, full-stack and testing.</p>
+                    <p>Built and deployed a web app using skills and knowledge across fundamentals of data structures, algorithms, testing, full-stack, and database programming.
+                      Collaborated on pair-programming assignments with cohort colleagues - daily lab projects include building databases, testing, and utilizing APIs.</p>
                   </div>
                 </div>
               </div>
@@ -28,7 +29,8 @@ const Experience = () => {
                   <h5 class="text-light-blue">November 2017 - June 2021</h5>
                   <h4>Standard Chartered Bank, US</h4>
                   <p>Sanctions Filtering Associate</p>
-                  <p>Identified and investigated possible matches related to sanctioned parties and countries, money laundering and terrorist financing organizations.</p>
+                  <p>Identified and investigated possible matches related to sanctioned parties and countries, money laundering, and terrorist financing using Fircosoft for Transaction Screening and analytical skills. 
+                    Increased average productivity of the team by personally exceeding 140% daily target transactions with 100% level of accuracy.</p>
                   </div>
                 </div>
               </div>
@@ -38,7 +40,8 @@ const Experience = () => {
                   <h5 class="text-light-blue">March 2014 - March 2016</h5>
                   <h4>Masan Consumer Corporation</h4>
                   <p>Executive Assistant</p>
-                  <p>Arranged and managed CEO's meeting calendar. Coordinated across various stakeholders to assist CEO in business operation.</p>
+                  <p>Arranged and managed CEO’s internal and external meeting calendar, as well as nationwide and regional travel schedule using time management, flexibility to adapt to unforeseeable changes, problem-solving and quick decision-making skills.
+                    Coordinated across various stakeholders to assist CEO in business operation to ensure alignment between company strategic directions and departmental execution using leadership, business acumen, and personable communication skills.</p>
                   </div>
                 </div>
               </div>
@@ -48,7 +51,9 @@ const Experience = () => {
                   <h5 class="text-light-blue">April 2011 - December 2013</h5>
                   <h4>EQuest Academy</h4>
                   <p>Academic Course Facilitator</p>
-                  <p>Built curriculum, recruited and trained English teachers.</p>
+                  <p>Assisted Academic Manager in curriculum building, material selection, and self-service library management. 
+                    Improved teacher qualification by recruiting teachers and providing training workshops.
+                    Taught interactive communication and test preparation courses (TOEFL, IELTS, SAT, GMAT).</p>
                   </div>
                 </div>
               </div>
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -13,8 +13,8 @@ const Testimonials = () => {
         
         <div class="carousel-indicators m-5">
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        {/* <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button> */}
         </div>
 
         <div class="carousel-inner d-flex justify-content-around pb-5">

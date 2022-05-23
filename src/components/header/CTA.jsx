0bb0d34b1f615748071d4cd4cc1cf0caef6df1fa
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-23 22:33:30 +0000

    Fixed resume open in new tab - changed directory

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import HoaiAnPhanResume from '../../assets/Hoai-An-Phan_Resume_2022.pdf'
+import HoaiAnPhanResume from './Hoai-An-Phan_Resume_2022.pdf'
 
-const onResumeDownload = () => {
-  window.open(HoaiAnPhanResume)
-}
+// const onResumeDownload = () => {
+//   window.open(HoaiAnPhanResume)
+// }
 const CTA = () => {
   return (
     <div class="my-5">
-      <button type="button" class="btn btn-light bg-navy me-1"
-              onClick={onResumeDownload}>Download Resume
-      </button>
+      <button type="button" class="btn btn-light bg-navy me-1"></button>
+        <a href={HoaiAnPhanResume} target="_blank" rel="noreferrer">Download Resume</a>
+      
       <button type="button" class="btn btn-light bg-light-blue border border-white ms-1">
         <a href="#contact">Let's Chat</a>
       </button>

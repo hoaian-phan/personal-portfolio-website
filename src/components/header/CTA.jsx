8e6be383aa3b93a44ba17af7c4a1176a1e7d6141
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-24 02:45:56 +0000

    Fixed resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -8,7 +8,7 @@ const CTA = () => {
   return (
     <div class="my-5">
       <button type="button" class="btn btn-light bg-navy me-1">
-        <a href={HoaiAnPhanResume} target="_blank" rel="noreferrer">Download Resume</a>
+        <a href={HoaiAnPhanResume} without rel="noopener noreferrer" target="_blank">Download Resume</a>
       </button>
       <button type="button" class="btn btn-light bg-light-blue border border-white ms-1">
         <a href="#contact">Let's Chat</a>

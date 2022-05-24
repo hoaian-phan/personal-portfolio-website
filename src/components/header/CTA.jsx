1619dffe98eb2bc371ab1a5bea740448fb9a83b7
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-24 03:34:51 +0000

    Uploaded resume to S3

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import HoaiAnPhanResume from './Hoai-An-Phan_Resume_2022.pdf'
-import { Document, Page, pdfjs } from "react-pdf"
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 
 const CTA = () => {
   return (
     <div class="my-5">
-      <button type="button" class="btn btn-light bg-navy me-1">
-        <a href={HoaiAnPhanResume} without rel="noopener noreferrer" target="_blank">Download Resume</a>
+      <button type="button" class="btn btn-light bg-navy me-1 text-light-blue">
+        <a href="https://hoaian-personal-website.s3.us-west-1.amazonaws.com/Hoai-An+Phan+Resume+May+2022.pdf" rel="noopener noreferrer" target="_blank">Download Resume</a>
       </button>
       <button type="button" class="btn btn-light bg-light-blue border border-white ms-1">
         <a href="#contact">Let's Chat</a>

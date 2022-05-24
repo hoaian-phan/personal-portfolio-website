a281c07de65330312fa8bfba77fb0314596c6048
Hoai An Phan <pnhoaian2904@gmail.com>
2022-05-24 21:41:15 +0000

    Added Cindy's testimonial

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -13,8 +13,8 @@ const Testimonials = () => {
         
         <div class="carousel-indicators m-5">
         <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        {/* <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button> */}
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        {/* <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button> */}
         </div>
 
         <div class="carousel-inner d-flex justify-content-around pb-5">
@@ -28,15 +28,15 @@ const Testimonials = () => {
             </div>
           </div>
 
-          {/* <div class="carousel-item">
+          <div class="carousel-item">
             <div class="border shadow bg-navy border-navy p-4 m-4 fix-width testimonial d-none d-md-block">
-              <p>An was one of those rare students who constantly reminded me why I went back to teaching in the first place. Her problem-solving ability and dedication to learning are incredible. She came into Hackbright Academy with some knowledge of basic Python and JavaScript syntax, and by the end of the course she had designed a sophisticated recommendation algorithm and taught herself Celery in less than a day. Her capstone project, Playdate Birdies, is one of the most impressive projects I have seen in nearly a year at Hackbright. An has the intelligence and passion to go really far as a software engineer.</p>
-              <h5>Jocelyn Tang</h5>
-              <h6>Software Engineering Instructor at Hackbright Academy</h6>
+              <p>I knew when I met An that she would be going places! Even with the responsibility as a mother with a small child, An was able to plan ahead and prepare so she could have the best experience as a learner.   I believe An is a natural born leader!  In many situations, she was named as being the person who helped another through solving a difficult problem, giving guidance and support. An has true grit and a tenacity like no other.  Many times was faced with adversity and overcame by quickly adapting and moving forward with solid planning skills.  An has self motivation and has the ability to motivate others as well.  She solves difficult problems with grace!  Sometimes even making it look easy!  I’m really excited to see An grow in a new role and career!  Where ever she lands, she will be an asset to the team and company!</p>
+              <h5>Cindy Hazelton</h5>
+              <h6>SEI Bootcamps Career Coach at Hackbright Academy</h6>
             </div>
           </div>
 
-          <div class="carousel-item">
+          {/* <div class="carousel-item">
             <div class="border shadow bg-navy border-navy p-4 m-4 fix-width testimonial d-none d-md-block">
               <p>An was one of those rare students who constantly reminded me why I went back to teaching in the first place. Her problem-solving ability and dedication to learning are incredible. She came into Hackbright Academy with some knowledge of basic Python and JavaScript syntax, and by the end of the course she had designed a sophisticated recommendation algorithm and taught herself Celery in less than a day. Her capstone project, Playdate Birdies, is one of the most impressive projects I have seen in nearly a year at Hackbright. An has the intelligence and passion to go really far as a software engineer.</p>
               <h5>Cindy Hazelton</h5>

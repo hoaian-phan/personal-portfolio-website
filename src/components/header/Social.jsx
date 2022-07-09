7098ff7ee28b5e7dee6e6142543c07a5d90ff3bf
Hoai An Phan <pnhoaian2904@gmail.com>
2022-07-09 06:19:26 +0000

    Added animation to Social

--- src/components/header/Social.jsx
+++ src/components/header/Social.jsx
@@ -6,11 +6,11 @@ import {AiOutlineMail} from 'react-icons/ai'
 const Social = () => {
   return (
     <div class="text-center mt-3">
-        <a href="https://linkedin.com/in/hoai-an-phan" target="_blank" rel="noreferrer"><GrLinkedin></GrLinkedin></a>
+        <a href="https://linkedin.com/in/hoai-an-phan" target="_blank" rel="noreferrer"><GrLinkedin class="social-icon"></GrLinkedin></a>
         &nbsp; &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp; &nbsp; 
-        <a href="https://github.com/hoaian-phan" target="_blank" rel="noreferrer"><GrGithub></GrGithub></a>
+        <a href="https://github.com/hoaian-phan" target="_blank" rel="noreferrer"><GrGithub class="social-icon"></GrGithub></a>
         &nbsp; &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp; &nbsp; 
-        <a href="mailto:hoaian.phan.sde2022@gmail.com" target="_blank" rel="noreferrer"><AiOutlineMail></AiOutlineMail></a>
+        <a href="mailto:hoaian.phan.sde2022@gmail.com" target="_blank" rel="noreferrer"><AiOutlineMail class="social-icon"></AiOutlineMail></a>
 
       </div>
   )
